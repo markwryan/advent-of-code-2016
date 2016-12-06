c12a7e34ff3d491d05ab11bce87f1174bfa2b792
Mark Ryan <markryan1988@gmail.com>
2016-12-05 23:11:52 -0500

    Working solution to part 5 that is so slow.

--- src/main/java/com/markwryan/day5/HowAboutANiceGameOfChess.java
+++ src/main/java/com/markwryan/day5/HowAboutANiceGameOfChess.java
@@ -2,6 +2,7 @@ package com.markwryan.day5;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.regex.Pattern;
 
 /**
  * Advent of Code 2015 Day 5 [SLOW AS BALLS]
@@ -14,29 +15,30 @@ public class HowAboutANiceGameOfChess {
         int index = 0;
         int found = 0;
         MessageDigest digest = MessageDigest.getInstance("MD5");
-
-        while(found < 8) {
+        StringBuilder sb;
+        Pattern pattern = Pattern.compile("00000[0-7].*");
+        while (found < 8) {
             byte[] hash = digest.digest((doorId + index).getBytes());
-            StringBuilder sb = new StringBuilder();
-            for(byte c : hash) {
+            sb = new StringBuilder();
+            for (byte c : hash) {
                 sb.append(String.format("%02X", c));
             }
-            String hex = sb.toString();
-            if(hex.startsWith("00000")) {
-                char p = hex.substring(6,7).charAt(0);
-                String pos = hex.substring(5,6);
-                if(pos.matches("[0-7]")) {
-                    int position = Integer.parseInt(pos);
-                    if(password[position] == null) {
-                        password[position] = p;
-                        System.out.println(position + ": " + p);
-                        found++;
-                    }
+            final String hex = sb.toString();
+            if (pattern.matcher(hex).matches()) {
+                char p = hex.charAt(6);
+                String pos = String.valueOf(hex.charAt(5));
+
+                int position = Integer.parseInt(pos);
+                if (password[position] == null) {
+                    password[position] = p;
+                    System.out.println(position + ": " + p);
+                    found++;
                 }
+
             }
             index++;
         }
-        for(char entry : password) {
+        for (char entry : password) {
             System.out.print(entry);
         }
     }

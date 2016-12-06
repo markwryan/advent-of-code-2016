b1a3fae2110731ca8b1a33150257905302482fa3
Mark Ryan <markryan1988@gmail.com>
2016-12-05 23:28:44 -0500

    fix slowness for day 5

--- src/main/java/com/markwryan/day5/HowAboutANiceGameOfChess.java
+++ src/main/java/com/markwryan/day5/HowAboutANiceGameOfChess.java
@@ -1,29 +1,25 @@
 package com.markwryan.day5;
 
+import javax.xml.bind.DatatypeConverter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.regex.Pattern;
 
 /**
- * Advent of Code 2015 Day 5 [SLOW AS BALLS]
+ * Advent of Code 2015 Day 5
  */
 public class HowAboutANiceGameOfChess {
-
     public static void main(String[] args) throws NoSuchAlgorithmException {
         String doorId = "wtnhxymk";
         Character[] password = new Character[8];
         int index = 0;
         int found = 0;
         MessageDigest digest = MessageDigest.getInstance("MD5");
-        StringBuilder sb;
         Pattern pattern = Pattern.compile("00000[0-7].*");
         while (found < 8) {
             byte[] hash = digest.digest((doorId + index).getBytes());
-            sb = new StringBuilder();
-            for (byte c : hash) {
-                sb.append(String.format("%02X", c));
-            }
-            final String hex = sb.toString();
+
+            final String hex = DatatypeConverter.printHexBinary(hash);
             if (pattern.matcher(hex).matches()) {
                 char p = hex.charAt(6);
                 String pos = String.valueOf(hex.charAt(5));
@@ -34,7 +30,6 @@ public class HowAboutANiceGameOfChess {
                     System.out.println(position + ": " + p);
                     found++;
                 }
-
             }
             index++;
         }
@@ -42,5 +37,4 @@ public class HowAboutANiceGameOfChess {
             System.out.print(entry);
         }
     }
-
 }
